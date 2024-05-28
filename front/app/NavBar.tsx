6910e3b74a3549a949141d43f542d86b24cc6063
sam <meisamtaher71@gmail.com>
2024-05-28 09:00:17 +0330

    change href to link

--- front/app/NavBar.tsx
+++ front/app/NavBar.tsx
@@ -12,13 +12,13 @@ const NavBar = () => {
     <nav className='flex space-x-6 mb-5 px-5 h-14 items-center bg-slate-950' >
         <div className='flex flex-row items-center space-x-2'>
           <img src="/logo.png" className='w-9 h-9'/>
-          <a href={"/"} className="text-white text-lg"> 8Pepen</a>
+          <Link href={"/"} className="text-white text-lg"> 8Pepen</Link>
         </div>
         <ul className='flex space-x-6'>
             {links.map(link=>
             <div className='flex flex-row space-x-1 items-center border border-gray-700 border-r-2 p-2' key={link.label}>
               {/* <img src={link.img}/> */}
-              <a className="text-white text-sm transition-colors" href={link.href} >{link.label}</a>
+              <Link className="text-white text-sm transition-colors" href={link.href} >{link.label}</Link>
             </div> 
             )}
         </ul>
