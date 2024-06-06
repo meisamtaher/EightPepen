19d033b1f53bf0acc3eaf2e4dbd3151e5a621877
sam <meisamtaher71@gmail.com>
2024-06-06 15:58:42 +0330

    add deployed Addresses

--- smartcontracts/scritps/03_deploySixteenPepenFCSetNFT.ts
+++ smartcontracts/scritps/03_deploySixteenPepenFCSetNFT.ts
@@ -2,10 +2,8 @@ import { ethers } from "hardhat";
 
 async function main() {
     const [owner] = await ethers.getSigners();
-    // 8Pepen Full Color Renderer deployed at address:  0x7aab67aaC3D1f5DE9EFF7ff8dF91Cc049AD0fEFe
-    // 8Pepen Renderer deployed at address:  0x7Ca1564B12B99f36A80a6A9f41137621481dcC42
-    // 8Pepen base NFT deployed at address:  0xAFfc13Ebd50B27ed7078A49A3d7eF7f52a862f4A
-    // 8 Pepen FC Set Contract Address:  0x7e8316844f7897C34c064Ae9C5b26c925b331545
+    // 16Pepen Full Color Renderer deployed at address:  0xA9E27F0fB49e5A89A678c6AB482D12d61fb21C1F
+    // 16 Pepen FC Set Contract Address:  0xe395ED69E670208bA0b5d3A7a24c26be542fDDAA
 
     const FCRendererContractFactory = await ethers.getContractFactory("SixteenPepenFCRenderer");
     const FCRenderer = await FCRendererContractFactory.deploy();
