6c65fdeea7180f65c66b0d7ed563cb1c9f242fd9
sam <meisamtaher71@gmail.com>
2024-06-25 16:16:50 +0330

    add Collaboration feature

--- smartcontracts/contracts/EightPepenFCNFT.sol
+++ smartcontracts/contracts/EightPepenFCNFT.sol
@@ -13,6 +13,7 @@ contract EightPepenFCNFT is ERC721 {
     IEightPepenFCRenderer public renderer;
     uint64 public setSupply;
     uint public imageSupply;
+    uint public collabReqCount;
     struct Set{
         string name;
         string description;
@@ -28,17 +29,20 @@ contract EightPepenFCNFT is ERC721 {
     } 
     struct TokenData{
         uint256 imageId;
+        uint256 secondId;
     }
     mapping(uint256 => Set) public sets;
     mapping(uint256 => uint32) public votes;
     mapping(uint256 => EightPepenFCData) public images;
     mapping(uint256 => TokenData) public tokens;
+    mapping(uint256 => TokenData) public collabReq;
 
     event AddImage(uint256 indexed _id, uint256 indexed _setId, uint16 count,address artist);
     event AddSet (uint256 indexed _id, address artist);
     event Opt_In(uint256 indexed _tokenId, uint256 indexed _imageId);
     event Published(uint256 indexed _id, uint256 indexed _setId, uint16 count,address artist);
-
+    event CollabReq(uint256 indexed _id, uint256 indexed firstToken, uint256 indexed secondToken);
+    event CollabAccept(uint256 indexed _id, uint256 indexed firstToken, uint256 indexed secondToken);
     constructor(IEightPepenFCRenderer _renderer)ERC721("Eight Pepen Full Color", "8PEPEN"){
         renderer = _renderer;
         EightPepenFCData memory firstImage;
@@ -102,6 +106,28 @@ contract EightPepenFCNFT is ERC721 {
         unsafe_opt_in(_imageId,_tokenId);
         emit Opt_In(_tokenId,_imageId);
     }
+    function collab_req(uint256 _ownedToken,uint256 _secondToken)public {
+        require(_ownedToken>0 && _ownedToken<=totalSupply,"token not found");
+        require(_secondToken>0 && _secondToken<=totalSupply,"token not found");
+        require(images[tokens[_ownedToken].imageId].revealed,"token not revealed");
+        require(images[tokens[_secondToken].imageId].revealed,"token not revealed");
+        require(ownerOf(_ownedToken)== msg.sender,"you are not the owner of token");
+        collabReqCount++;
+        collabReq[collabReqCount].imageId = _ownedToken;
+        collabReq[collabReqCount].secondId = _secondToken;
+        emit CollabReq(collabReqCount,_ownedToken,_secondToken);
+    }
+    function accept_req(uint256 _collabReqId)public{
+        //check collab_req exist 
+        // check ownership 
+        require(_collabReqId>0 && _collabReqId<=collabReqCount, "collab not found");
+        TokenData memory collab = collabReq[_collabReqId];
+        require(ownerOf(collab.secondId)== msg.sender,"you are not the owner of token");
+        tokens[collab.imageId].secondId = tokens[collab.secondId].imageId;
+        tokens[collab.secondId].secondId = tokens[collab.imageId].imageId;
+        emit CollabAccept(_collabReqId,collab.imageId,collab.secondId);
+
+    }
     function unsafe_opt_in(uint256 _imageId,uint256 _tokenId) internal {
         tokens[_tokenId].imageId = _imageId;
         votes[_imageId] ++;
@@ -113,9 +139,36 @@ contract EightPepenFCNFT is ERC721 {
     function tokenURI(uint tokenId)public view override returns (string memory){
         require(tokenId>0 && tokenId<=totalSupply,"Invalid tokenId");
         uint256 imageId = images[tokens[tokenId].imageId].revealed?tokens[tokenId].imageId:1;
-        return imageURI(imageId);
+        return tokens[tokenId].secondId ==0? imageURI(imageId,tokenId) : imageURI(imageId,tokens[tokenId].secondId,tokenId);
     }
-    function imageURI(uint _imageId)public view returns (string memory){
+    function imageURI(uint _imageId1,uint _imageId2, uint _tokenId)public view returns(string memory){
+        require(_imageId1>0 && _imageId1<=imageSupply,"Invalid imageId");
+        require(_imageId2>0 && _imageId2<=imageSupply,"Invalid imageId");
+        IEightPepenFCRenderer customRenderer = (_imageId1!=0 && sets[images[_imageId1].setId].hasRenderer)?sets[images[_imageId1].setId].renderer:renderer;
+        return
+            string(
+                abi.encodePacked(
+                    "data:application/json;base64,",
+                    Base64.encode(
+                        bytes(
+                            abi.encodePacked(
+                                '{"name": "Eight Pepen #',
+                                Strings.toString(_tokenId),
+                                '", "description": "Eight Pepen is a collection of 888 fully on-chain, randomly generated'
+                                '", "image":"data:image/svg+xml;base64,',
+                                Base64.encode(bytes(customRenderer.getSVG(images[_imageId1].pixelColors,images[_imageId1].bgColor,images[_imageId2].pixelColors,images[_imageId2].bgColor))),
+                                // '", "attributes": ""',
+                                // '}'
+                                '", "attributes": [',
+                                getAttributes(_imageId1),
+                                ']}'
+                            )
+                        )
+                    )
+                )
+            );
+    }
+    function imageURI(uint _imageId,uint _tokenId)public view returns (string memory){
         require(_imageId>0 && _imageId<=imageSupply,"Invalid imageId");
         IEightPepenFCRenderer customRenderer = (_imageId!=0 && sets[images[_imageId].setId].hasRenderer)?sets[images[_imageId].setId].renderer:renderer;
         return
@@ -126,7 +179,7 @@ contract EightPepenFCNFT is ERC721 {
                         bytes(
                             abi.encodePacked(
                                 '{"name": "Eight Pepen #',
-                                Strings.toString(_imageId),
+                                Strings.toString(_tokenId),
                                 '", "description": "Eight Pepen is a collection of 888 fully on-chain, randomly generated'
                                 '", "image":"data:image/svg+xml;base64,',
                                 Base64.encode(bytes(customRenderer.getSVG(images[_imageId].pixelColors,images[_imageId].bgColor))),
--- smartcontracts/scritps/02_deployEightPepenFCSetNFT.ts
+++ smartcontracts/scritps/02_deployEightPepenFCSetNFT.ts
@@ -7,10 +7,10 @@ async function main() {
     // 8Pepen base NFT deployed at address:  0xAFfc13Ebd50B27ed7078A49A3d7eF7f52a862f4A
     // 8 Pepen FC Set Contract Address:  0x7e8316844f7897C34c064Ae9C5b26c925b331545
     // 8 pepen FC Set Contract Address with set : 0xA3D1F0A6c7483489FAa10f3F9b1A5DA5AFa4144e
-    // const FCRendererContractFactory = await ethers.getContractFactory("EightPepenFCRenderer");
-    // const FCRenderer = await FCRendererContractFactory.deploy();
-    // await FCRenderer.waitForDeployment();
-    const FCRendererAddress = "0x7aab67aaC3D1f5DE9EFF7ff8dF91Cc049AD0fEFe"
+    const FCRendererContractFactory = await ethers.getContractFactory("EightPepenFCRenderer");
+    const FCRenderer = await FCRendererContractFactory.deploy();
+    await FCRenderer.waitForDeployment();
+    const FCRendererAddress = await FCRenderer.getAddress();
     console.log(" 8Pepen Full Color Renderer deployed at address: ", FCRendererAddress);
 
     const SetNFTContractFactory = await ethers.getContractFactory("EightPepenFCNFT");
