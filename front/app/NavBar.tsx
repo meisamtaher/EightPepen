153e927196bcae45dccac71b7404f3ea0e13ae82
sam <meisamtaher71@gmail.com>
2024-06-29 02:09:17 +0330

    fix style

--- front/app/NavBar.tsx
+++ front/app/NavBar.tsx
@@ -18,14 +18,18 @@ const NavBar = () => {
     console.log("account: ", account)
   }, [account]);
   return (
-    <nav className='flex space-x-6 mb-5 px-5 h-20 items-center bg-slate-950'  >
-        <div className='flex flex-row items-center space-x-2'>
-          <img src="/logo.png" className='w-9 h-9'/>
-          <Link href={"/"} className="text-white text-lg"> 8Pepen</Link>
+    <nav className='flex space-x-6  px-2 h-16 items-center bg-slate-950 gap-12'  >
+        <div className='flex flex-row items-center space-x-1.5'>
+          <img src="/logo.png" className='w-16 h-16'/>
+          <Link href={"/"} > 
+            <div className="text-white text-lg">
+              8Pepen
+            </div>
+          </Link>
         </div>
-        <ul className='flex space-x-6'>
+        <ul className='flex flex-row space-x-3 p-1'>
             {links.map(link=>
-            <div className='flex flex-row space-x-1 items-center border border-gray-700 border-r-2 p-2' key={link.label}>
+            <div className='flex flex-row space-x-1 items-center border border-gray-700  border-r-2 p-2' key={link.label}>
               {/* <img src={link.img}/> */}
               <Link className="text-white text-sm transition-colors" href={link.href} >{link.label}</Link>
             </div> 
@@ -35,8 +39,6 @@ const NavBar = () => {
          {!account.isConnected && <BlackCreateWalletButton></BlackCreateWalletButton>}
           <ConnectButton ></ConnectButton>
         </div>
-        
-        
     </nav>
   )
 }
