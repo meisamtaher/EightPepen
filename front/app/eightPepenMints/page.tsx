00a0a47356874a37d33519dc0c48484ebf901569
sam <meisamtaher71@gmail.com>
2024-07-28 00:37:18 +0330

    change font sizes to 8

--- front/app/eightPepenMints/page.tsx
+++ front/app/eightPepenMints/page.tsx
@@ -33,22 +33,22 @@ const EightPepenMint = () => {
     }, []);
   return (
     <div className='flex flex-col gap-5  pt-10 p-x-2'>
-      <a className=' text-lg'>JOIN THE WEB3 REVOLUTION</a>
-      <a className=' text-xs'>
+      <a className=' text-base'>JOIN THE WEB3 REVOLUTION</a>
+      <a className=' text-[0.5rem]'>
         Mint an unrevealed 8pepen token to become a collector & curator. 
       </a>
-      <a className=' text-xs'> With your unrevealed token you can start opting in to a sets of your choice and shape actively the 8pepen collection.</a>
-      <a className=' text-xs'> When enough collectors are interested in the set, a consensus is met and the metadata of your unrevealed token will change to the artwork you opted in.</a>
-      <a className=' text-xs'>We start with a first-come first-serve principal</a>
-      <a className='mt-10 text-lg' >8✖️8 Contract</a>
+      <a className=' text-[0.5rem]'> With your unrevealed token you can start opting in to a sets of your choice and shape actively the 8pepen collection.</a>
+      <a className=' text-[0.5rem]'> When enough collectors are interested in the set, a consensus is met and the metadata of your unrevealed token will change to the artwork you opted in.</a>
+      <a className=' text-[0.5rem]'>We start with a first-come first-serve principal</a>
+      <a className='mt-10 text-base' >8✖️8 Contract</a>
       <div className='border-t-4 border-black' />
       <div className='flex flex-wrap  gap-20 p-4'>
         <div className='flex flex-col gap-5'>
-          <div className='flex felx-row  gap-4 text-xs'><a> mint Price: </a><a className='text-xs font-extrabold'> {mintPrice} Eth</a></div>
-          <div className='flex flex-row tex-xs'> {totalSuply}/{maxSupply}</div>
+          <div className='flex felx-row  gap-4 text-[0.5rem]'><a> mint Price: </a><a className='text-[0.5rem] font-extrabold'> {mintPrice} Eth</a></div>
+          <div className='flex flex-row text-[0.5rem]'> {totalSuply}/{maxSupply}</div>
         </div>
         <div className='grid flex-1 justify-items-center'>
-          <button className='text-lg bg-black  text-white px-8 py-4' onClick={mintEightPepen}> Mint</button>
+          <button className='text-base bg-black  text-white px-8 py-4' onClick={mintEightPepen}> Mint</button>
         </div>
       </div>
     </div>
